a39d1868f7f730b46ae639ad7c91c344662e849d
Julian Orth <ju.orth@gmail.com>
2022-06-06 17:14:52 +0200

    xwayland: destroy stack_link on window destroy

--- src/xwayland/xwm.rs
+++ src/xwayland/xwm.rs
@@ -1882,6 +1882,7 @@ impl Wm {
         };
         // log::info!("xwm destroy_notify {}", event.window);
         data.destroyed.set(true);
+        data.stack_link.borrow_mut().take();
         if let Some(sid) = data.surface_id.take() {
             self.windows_by_surface_id.remove(&sid);
         }
