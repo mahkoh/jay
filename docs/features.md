5dff6c38fd185b3ca30c3b99169db7f949426f07
Julian Orth <ju.orth@gmail.com>
2024-04-24 13:51:13 +0200

    wayland: implement xdg-dialog-v1

--- docs/features.md
+++ docs/features.md
@@ -148,6 +148,7 @@ Jay supports the following wayland protocols:
 | xdg_activation_v1                       | 1                |               |
 | xdg_toplevel_drag_manager_v1            | 1                |               |
 | xdg_wm_base                             | 6                |               |
+| xdg_wm_dialog_v1                        | 1                |               |
 | zwlr_data_control_manager_v1            | 2                | Yes           |
 | zwlr_layer_shell_v1                     | 4[^no_exclusive] | No[^lsaccess] |
 | zwlr_screencopy_manager_v1              | 3                | Yes           |
---
+++ src/ifs/wl_surface/xdg_surface/xdg_toplevel/xdg_dialog_v1.rs
@@ -0,0 +1,73 @@
+use {
+    crate::{
+        client::{Client, ClientError},
+        ifs::wl_surface::xdg_surface::xdg_toplevel::XdgToplevel,
+        leaks::Tracker,
+        object::{Object, Version},
+        wire::{xdg_dialog_v1::*, XdgDialogV1Id, XdgToplevelId},
+    },
+    std::{fmt::Debug, rc::Rc},
+    thiserror::Error,
+};
+
+pub struct XdgDialogV1 {
+    pub id: XdgDialogV1Id,
+    pub client: Rc<Client>,
+    pub toplevel: Rc<XdgToplevel>,
+    pub tracker: Tracker<Self>,
+    pub version: Version,
+}
+
+impl XdgDialogV1 {
+    fn detach(&self) {
+        self.toplevel.dialog.take();
+    }
+
+    pub fn install(self: &Rc<Self>) -> Result<(), XdgDialogV1Error> {
+        if self.toplevel.dialog.is_some() {
+            return Err(XdgDialogV1Error::AlreadyAttached(self.toplevel.id));
+        }
+        self.toplevel.dialog.set(Some(self.clone()));
+        Ok(())
+    }
+}
+
+impl XdgDialogV1RequestHandler for XdgDialogV1 {
+    type Error = XdgDialogV1Error;
+
+    fn destroy(&self, _req: Destroy, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        self.detach();
+        self.client.remove_obj(self)?;
+        Ok(())
+    }
+
+    fn set_modal(&self, _req: SetModal, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn unset_modal(&self, _req: UnsetModal, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+object_base! {
+    self = XdgDialogV1;
+    version = self.version;
+}
+
+impl Object for XdgDialogV1 {
+    fn break_loops(&self) {
+        self.detach();
+    }
+}
+
+simple_add_obj!(XdgDialogV1);
+
+#[derive(Debug, Error)]
+pub enum XdgDialogV1Error {
+    #[error(transparent)]
+    ClientError(Box<ClientError>),
+    #[error("Toplevel {0} already has an xdg_dialog_v1")]
+    AlreadyAttached(XdgToplevelId),
+}
+efrom!(XdgDialogV1Error, ClientError);
