e1d1fe7fda34eb8bb7f1ba722c840008e27fcefe
Julian Orth <ju.orth@gmail.com>
2024-09-07 17:02:28 +0200

    util: add OnDrop2 for non-copy closures

--- src/utils/on_drop.rs
+++ src/utils/on_drop.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::{mem, mem::ManuallyDrop};
 
 pub struct OnDrop<F>(pub F)
 where
@@ -13,5 +13,36 @@ impl<F: FnMut() + Copy> OnDrop<F> {
 impl<F: FnMut() + Copy> Drop for OnDrop<F> {
     fn drop(&mut self) {
         (self.0)();
+    }
+}
+
+pub struct OnDrop2<F>
+where
+    F: FnOnce(),
+{
+    f: ManuallyDrop<F>,
+}
+
+impl<F: FnOnce()> OnDrop2<F> {
+    #[expect(dead_code)]
+    pub fn new(f: F) -> Self {
+        Self {
+            f: ManuallyDrop::new(f),
+        }
+    }
+
+    #[expect(dead_code)]
+    pub fn forget(mut self) {
+        unsafe {
+            ManuallyDrop::drop(&mut self.f);
+        }
+        mem::forget(self);
+    }
+}
+
+impl<F: FnOnce()> Drop for OnDrop2<F> {
+    fn drop(&mut self) {
+        let f = unsafe { ManuallyDrop::take(&mut self.f) };
+        f();
     }
 }
