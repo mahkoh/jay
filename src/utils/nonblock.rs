3f001588b8d58851c315b6e08e39454a6bec35b0
Julian Orth <ju.orth@gmail.com>
2024-10-19 08:58:29 +0200

    metal: unset O_NONBLOCK for DRM file descriptors

--- src/utils/nonblock.rs
+++ src/utils/nonblock.rs
@@ -2,6 +2,12 @@ use {crate::utils::oserror::OsError, uapi::c};
 
 pub fn set_nonblock(fd: c::c_int) -> Result<(), OsError> {
     let fl = uapi::fcntl_getfl(fd)?;
-    uapi::fcntl_setfl(fd, fl | c::SOCK_NONBLOCK)?;
+    uapi::fcntl_setfl(fd, fl | c::O_NONBLOCK)?;
+    Ok(())
+}
+
+pub fn set_block(fd: c::c_int) -> Result<(), OsError> {
+    let fl = uapi::fcntl_getfl(fd)?;
+    uapi::fcntl_setfl(fd, fl & !c::O_NONBLOCK)?;
     Ok(())
 }
