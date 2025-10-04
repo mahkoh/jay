ee84b309baaac9c3d1ab4f3fe9a7d95140d9c795
Julian Orth <ju.orth@gmail.com>
2025-10-04 22:32:59 +0200

    build.rs: don't build compile-shaders if shaders are unchanged

--- compile-shaders/core/src/lib.rs
+++ compile-shaders/core/src/lib.rs
@@ -1,36 +1,6 @@
-use {std::fmt::Write, walkdir::WalkDir};
+include!("../../../build/vulkan/hash.rs");
 
-pub const ROOT: &str = "src/gfx_apis/vulkan/shaders";
 pub const BIN: &str = "src/gfx_apis/vulkan/shaders_bin";
-pub const HASH: &str = "src/gfx_apis/vulkan/shaders_hash.txt";
-
-fn calculate_hash() -> anyhow::Result<String> {
-    let dir = WalkDir::new(ROOT);
-    let mut files = vec![];
-    for file in dir {
-        let file = file?;
-        if file.file_type().is_file() {
-            files.push(file.path().to_path_buf());
-        }
-    }
-    files.sort();
-    let mut out = String::new();
-    for file in files {
-        let data = std::fs::read(&file)?;
-        writeln!(out, "{} {}", blake3::hash(&data).to_hex(), file.display())?;
-    }
-    Ok(out)
-}
-
-pub fn unchanged() -> bool {
-    let Ok(actual) = std::fs::read_to_string(HASH) else {
-        return false;
-    };
-    let Ok(expected) = calculate_hash() else {
-        return false;
-    };
-    actual == expected
-}
 
 pub fn update_hash() -> anyhow::Result<()> {
     std::fs::write(HASH, calculate_hash()?)?;
