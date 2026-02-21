ce3090109359d938a13edae3035a2a8d83f91eaf
Julian Orth <ju.orth@gmail.com>
2026-02-22 00:11:31 +0100

    wl_usr: add additional interfaces

---
+++ src/wl_usr/usr_ifs/usr_wl_data_offer.rs
@@ -0,0 +1,58 @@
+use {
+    crate::{
+        object::Version,
+        wire::{WlDataOfferId, wl_data_offer::*},
+        wl_usr::{UsrCon, usr_object::UsrObject},
+    },
+    ahash::AHashSet,
+    std::{cell::RefCell, convert::Infallible, rc::Rc},
+    uapi::OwnedFd,
+};
+
+pub struct UsrWlDataOffer {
+    pub id: WlDataOfferId,
+    pub con: Rc<UsrCon>,
+    pub version: Version,
+    pub mime_types: RefCell<AHashSet<String>>,
+}
+
+impl UsrWlDataOffer {
+    #[expect(dead_code)]
+    pub fn receive(&self, mime_type: &str, fd: &Rc<OwnedFd>) {
+        self.con.request(Receive {
+            self_id: self.id,
+            mime_type,
+            fd: fd.clone(),
+        });
+    }
+}
+
+impl WlDataOfferEventHandler for UsrWlDataOffer {
+    type Error = Infallible;
+
+    fn offer(&self, ev: Offer<'_>, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        self.mime_types
+            .borrow_mut()
+            .insert(ev.mime_type.to_string());
+        Ok(())
+    }
+
+    fn source_actions(&self, _ev: SourceActions, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn action(&self, _ev: Action, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+usr_object_base! {
+    self = UsrWlDataOffer = WlDataOffer;
+    version = self.version;
+}
+
+impl UsrObject for UsrWlDataOffer {
+    fn destroy(&self) {
+        self.con.request(Destroy { self_id: self.id });
+    }
+}
---
+++ src/wl_usr/usr_ifs/usr_xdg_toplevel.rs
@@ -0,0 +1,95 @@
+use {
+    crate::{
+        object::Version,
+        utils::clonecell::CloneCell,
+        wire::{WlOutputId, XdgToplevelId, xdg_toplevel::*},
+        wl_usr::{UsrCon, usr_object::UsrObject},
+    },
+    std::{convert::Infallible, rc::Rc},
+};
+
+pub struct UsrXdgToplevel {
+    pub id: XdgToplevelId,
+    pub con: Rc<UsrCon>,
+    pub owner: CloneCell<Option<Rc<dyn UsrXdgToplevelOwner>>>,
+    pub version: Version,
+}
+
+impl UsrXdgToplevel {
+    #[expect(dead_code)]
+    pub fn set_title(&self, title: &str) {
+        self.con.request(SetTitle {
+            self_id: self.id,
+            title,
+        });
+    }
+
+    #[expect(dead_code)]
+    pub fn set_fullscreen(&self, fullscreen: bool) {
+        match fullscreen {
+            true => {
+                self.con.request(SetFullscreen {
+                    self_id: self.id,
+                    output: WlOutputId::NONE,
+                });
+            }
+            false => {
+                self.con.request(UnsetFullscreen { self_id: self.id });
+            }
+        }
+    }
+}
+
+pub trait UsrXdgToplevelOwner {
+    fn configure(&self, width: i32, height: i32) {
+        let _ = width;
+        let _ = height;
+    }
+
+    fn close(&self) {
+        // nothing
+    }
+}
+
+impl UsrXdgToplevel {}
+
+impl XdgToplevelEventHandler for UsrXdgToplevel {
+    type Error = Infallible;
+
+    fn configure(&self, ev: Configure<'_>, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        if let Some(owner) = self.owner.get() {
+            owner.configure(ev.width, ev.height);
+        }
+        Ok(())
+    }
+
+    fn close(&self, _ev: Close, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        if let Some(owner) = self.owner.get() {
+            owner.close();
+        }
+        Ok(())
+    }
+
+    fn configure_bounds(&self, _ev: ConfigureBounds, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn wm_capabilities(&self, _ev: WmCapabilities<'_>, _slf: &Rc<Self>) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+usr_object_base! {
+    self = UsrXdgToplevel = XdgToplevel;
+    version = self.version;
+}
+
+impl UsrObject for UsrXdgToplevel {
+    fn destroy(&self) {
+        self.con.request(Destroy { self_id: self.id })
+    }
+
+    fn break_loops(&self) {
+        self.owner.take();
+    }
+}
