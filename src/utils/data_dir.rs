a9df8799799549bc149c9fd220314f1ea25163eb
Stipe Kotarac <stipe@kotarac.net>
2026-04-16 13:41:07 +0200

    it: data dir in testruns

--- src/utils/data_dir.rs
+++ src/utils/data_dir.rs
@@ -3,6 +3,14 @@ use std::{
     sync::LazyLock,
 };
 
+#[cfg(feature = "it")]
+pub fn data_dir() -> &'static Path {
+    static DIR: LazyLock<PathBuf> =
+        LazyLock::new(|| Path::new(env!("CARGO_MANIFEST_DIR")).join("testruns/data"));
+    &DIR
+}
+
+#[cfg(not(feature = "it"))]
 pub fn data_dir() -> &'static Path {
     static DIR: LazyLock<PathBuf> = LazyLock::new(|| {
         let Some(mut dir) = dirs::data_local_dir() else {
