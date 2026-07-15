08022cc8a91640d6aa3d5efd27ad8d8d8033f665
Julian Orth <ju.orth@gmail.com>
2026-07-15 17:57:37 +0200

    codegen: generate config keysyms

---
+++ codegen/src/keysyms.rs
@@ -0,0 +1,27 @@
+use {anyhow::Result, kbvm::Keysym, std::fmt::Write};
+
+pub fn main() -> Result<()> {
+    let mut syms = vec![];
+    for sym in Keysym::all() {
+        syms.push((sym.name().unwrap(), sym));
+    }
+    syms.sort_by_cached_key(|s| s.0.to_string() + ":");
+    let mut res = String::new();
+    writeln!(res, r#"use super::KeySym;"#)?;
+    writeln!(res)?;
+    for (name, sym) in syms {
+        writeln!(
+            res,
+            r#"pub const SYM_{name}: KeySym = KeySym(0x{:x});"#,
+            sym.0
+        )?;
+    }
+    std::fs::write(
+        concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/../jay-config/src/keyboard/syms/generated.rs",
+        ),
+        res,
+    )?;
+    Ok(())
+}
