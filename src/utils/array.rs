d608a333a2e3307f83632dbdd14a82ef7038eaff
Julian Orth <ju.orth@gmail.com>
2026-08-08 17:41:58 +0200

    utils: remove SliceExt

--- src/utils/array.rs
+++ src/utils/array.rs
@@ -9,32 +9,3 @@ where
         res
     })
 }
-
-#[expect(unused)]
-pub trait SliceExt {
-    type T;
-
-    fn slice_array<const N: usize>(&self, lo: usize) -> Option<&[Self::T; N]>;
-    fn split_array<const N: usize>(&self) -> Option<(&[Self::T; N], &[Self::T])>;
-}
-
-impl<T> SliceExt for [T] {
-    type T = T;
-
-    fn slice_array<const N: usize>(&self, lo: usize) -> Option<&[Self::T; N]> {
-        if lo > self.len() || self.len() - lo < N {
-            return None;
-        }
-        let array = unsafe { &*self.as_ptr().add(lo).cast::<[Self::T; N]>() };
-        Some(array)
-    }
-
-    fn split_array<const N: usize>(&self) -> Option<(&[Self::T; N], &[Self::T])> {
-        if self.len() < N {
-            return None;
-        }
-        let array = unsafe { &*self.as_ptr().cast::<[Self::T; N]>() };
-        let tail = unsafe { self.get_unchecked(N..) };
-        Some((array, tail))
-    }
-}
