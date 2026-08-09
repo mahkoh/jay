d386e1d8c894a023c4e6a65e568236c93e8de14e
Julian Orth <ju.orth@gmail.com>
2026-08-09 04:38:06 +0200

    utils: remove MaybeUninitSliceExt1

--- src/utils/maybe_uninit.rs
+++ src/utils/maybe_uninit.rs
@@ -2,25 +2,6 @@ use std::mem;
 use std::mem::MaybeUninit;
 
 #[expect(unused)]
-pub trait MaybeUninitSliceExt1 {
-    type T;
-
-    fn init<const N: usize>(&mut self, val: [Self::T; N]) -> &mut [Self::T; N];
-}
-
-impl<T> MaybeUninitSliceExt1 for [MaybeUninit<T>] {
-    type T = T;
-
-    fn init<const N: usize>(&mut self, val: [Self::T; N]) -> &mut [Self::T; N] {
-        unsafe {
-            let slf: *mut [Self::T; N] = self[..N].as_mut_ptr() as _;
-            std::ptr::write(slf, val);
-            &mut *slf
-        }
-    }
-}
-
-#[expect(unused)]
 pub trait MaybeUninitSliceExt2<const N: usize> {
     fn cast_mut<U>(&mut self) -> &mut [MaybeUninit<U>; N];
 }
