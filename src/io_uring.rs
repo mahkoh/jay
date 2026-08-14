6491ebc2c2df3ec4e3c69d5407c851c19e9da041
Julian Orth <ju.orth@gmail.com>
2026-08-14 13:29:05 +0200

    io_uring: add support for multishot tasks

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -17,6 +17,7 @@ use crate::io_uring::ops::timeout::TimeoutTask;
 use crate::io_uring::ops::timeout_external::TimeoutExternalTask;
 use crate::io_uring::ops::timeout_link::TimeoutLinkTask;
 use crate::io_uring::pending_result::PendingResults;
+use crate::io_uring::sys::IORING_CQE_F_MORE;
 use crate::io_uring::sys::IORING_ENTER_GETEVENTS;
 use crate::io_uring::sys::IORING_FEAT_NODROP;
 use crate::io_uring::sys::IORING_OFF_CQ_RING;
@@ -265,6 +266,7 @@ impl IoUring {
             fd_ids_scratch: Default::default(),
             iteration: Default::default(),
             yields: Default::default(),
+            multishot_tasks: Default::default(),
         });
         Ok(Rc::new(Self { ring: data }))
     }
@@ -320,6 +322,7 @@ struct IoUringData {
     to_encode: SyncQueue<IoUringTaskId>,
     pending_in_kernel: CopyHashMap<IoUringTaskId, (), FxBuildHasher>,
     tasks: CopyHashMap<IoUringTaskId, TaskPlus, FxBuildHasher>,
+    multishot_tasks: CopyHashMap<IoUringTaskId, Rc<dyn MultishotTask>, FxBuildHasher>,
 
     pending_results: PendingResults,
 
@@ -349,6 +352,7 @@ struct IoUringData {
 struct TaskPlus {
     task: Box<dyn Task>,
     has_timeout: bool,
+    multishot: bool,
 }
 
 unsafe trait Task: 'static {
@@ -363,6 +367,14 @@ unsafe trait Task: 'static {
     fn has_timeout(&self) -> bool {
         false
     }
+
+    fn multishot(&self) -> Option<Rc<dyn MultishotTask>> {
+        None
+    }
+}
+
+unsafe trait MultishotTask: 'static {
+    fn complete_partial(self: Rc<Self>, cqe: &io_uring_cqe);
 }
 
 impl IoUringData {
@@ -434,9 +446,18 @@ impl IoUringData {
                 head = head.wrapping_add(1);
                 self.cqhead.deref().store(head, Release);
                 let id = IoUringTaskId(entry.user_data);
-                if let Some(pending) = self.tasks.remove(&id) {
-                    self.pending_in_kernel.remove(&id);
-                    pending.task.complete(self, &entry);
+                if entry.flags.contains(IORING_CQE_F_MORE) {
+                    if let Some(pending) = self.multishot_tasks.get(&id) {
+                        pending.complete_partial(&entry);
+                    }
+                } else {
+                    if let Some(pending) = self.tasks.remove(&id) {
+                        if pending.multishot {
+                            self.multishot_tasks.remove(&id);
+                        }
+                        self.pending_in_kernel.remove(&id);
+                        pending.task.complete(self, &entry);
+                    }
                 }
             }
             self.cqhead.deref().store(head, Release);
@@ -505,24 +526,40 @@ impl IoUringData {
                 res: -c::ECANCELED,
                 flags: 0,
             };
-            self.tasks.remove(&id).unwrap().task.complete(self, &cqe);
+            let task = self.tasks.remove(&id).unwrap();
+            if task.multishot {
+                self.multishot_tasks.remove(&id);
+            }
+            task.task.complete(self, &cqe);
             return;
         }
         self.cancel_task_in_kernel(id);
     }
 
     fn schedule(&self, t: Box<impl Task>) {
-        self.schedule_(t.id(), t.has_timeout(), t);
+        self.schedule_(t.id(), t.has_timeout(), t.multishot(), t);
     }
 
-    fn schedule_(&self, id: IoUringTaskId, has_timeout: bool, t: Box<dyn Task>) {
+    fn schedule_(
+        &self,
+        id: IoUringTaskId,
+        has_timeout: bool,
+        m: Option<Rc<dyn MultishotTask>>,
+        t: Box<dyn Task>,
+    ) {
         assert!(!self.destroyed.get());
+        let mut multishot = false;
+        if let Some(m) = m {
+            self.multishot_tasks.set(id, m);
+            multishot = true;
+        }
         self.to_encode.push(id);
         self.tasks.set(
             id,
             TaskPlus {
                 task: t,
                 has_timeout,
+                multishot,
             },
         );
     }
