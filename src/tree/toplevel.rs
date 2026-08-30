785c6e6a871a1a27e52163e9b6554cb48322ea5b
Julian Orth <ju.orth@gmail.com>
2026-08-30 15:27:56 +0200

    toplevel: simplify toplevel_parent_container

--- src/tree/toplevel.rs
+++ src/tree/toplevel.rs
@@ -1180,10 +1180,8 @@ pub fn default_tile_drag_bounds<T: ToplevelNodeBase + ?Sized>(t: &T, split: Cont
 }
 
 pub fn toplevel_parent_container(tl: &dyn ToplevelNode) -> Option<Rc<ContainerNode>> {
-    if let Some(parent) = tl.tl_data().parent.get()
-        && let Some(container) = parent.node_into_container()
-    {
-        return Some(container);
+    if let Some(parent) = tl.tl_data().parent.get() {
+        return parent.node_into_container();
     }
     None
 }
